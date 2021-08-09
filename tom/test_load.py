11796e06ee88995b1e9823d7147057885f9ad6de
Tom Casaletto <tom.casaletto@gmail.com>
2021-08-08 21:59:53 -0500

    Added title, and 2 plots, one with target=1 and one with target=0

--- tom/test_load.py
+++ tom/test_load.py
@@ -29,6 +29,7 @@ print(train.head())
 print(test.head())
 
 # Let's plot the time series from the first training example
+print('First example')
 print(train.iloc[0][2])
 data = np.load(train.iloc[0][2])
 
@@ -53,6 +54,27 @@ plt.ylabel('signal')
 plt.grid(True)
 plt.axis('tight')
 plt.legend(loc='upper left')
+plt.title('File: ' + train.iloc[0][2] + ', target=' + str(train.iloc[0][1]))
+
+
+print('Second example')
+print(train.iloc[1][2])
+data = np.load(train.iloc[1][2])
+
+plt.figure(2)
+plt.clf()
+idx = range(0, 4096)
+#for series in range(3):
+#    plt.plot(idx, data[series][:])
+plt.plot(idx, data[0][:], label='Hanford')
+plt.plot(idx, data[1][:], label='Livingston')
+plt.plot(idx, data[2][:], label='Virgo')
+
+plt.xlabel('time (idx)')
+plt.ylabel('signal')
+plt.grid(True)
+plt.axis('tight')
+plt.legend(loc='upper left')
+plt.title('File: ' + train.iloc[1][2] + ', target=' + str(train.iloc[1][1]))
 
 plt.show()
-print(data[0][:])
